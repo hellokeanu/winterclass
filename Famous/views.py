431c59f3ea39c51767ed6f8520c2824109e6bc76
hellokeanu <hello.keanu@gmail.com>
2021-02-09 14:26:28 +0900

    2021-02-09 14:26

--- Famous/views.py
+++ Famous/views.py
@@ -4,4 +4,23 @@ from django.http import HttpResponse
 
 def index(request):
     print("Hello")
-    return HttpResponse("Hello")
+    #return HttpResponse("Hello")
+    return render(request, 'Famous/index.html')
+
+
+
+def restaurantDetail(request):
+    # return HttpResponse("Hello")
+    return render(request, 'Famous/restaurantDetail.html')
+
+
+
+def restaurantCreate(request):
+    ## return HttpResponse("Hello")
+    return render(request, 'Famous/restaurantCreate.html')
+
+
+
+def categoryCreate(request):
+    ##return HttpResponse("Hello")
+    return render(request, 'Famous/categoryCreate.html')
